2c581db5110050246f47da8bb0faa62212c2ec8a
Chris <135656550+chris125991@users.noreply.github.com>
2026-03-24 06:48:09 -0400

    Add files via upload

---
+++ Windows_11_25H2_v1.4/Remove Features/Remove_Features.bat
@@ -0,0 +1,6 @@
+Dism /online /Disable-Feature /FeatureName:Printing-XPSServices-Features /NoRestart
+Dism /online /Disable-Feature /FeatureName:WindowsMediaPlayer /NoRestart
+Dism /online /Disable-Feature /FeatureName:Printing-PrintToPDFServices-Features /NoRestart
+Dism /online /Disable-Feature /FeatureName:WorkFolders-Client /NoRestart
+Dism /online /Disable-Feature /FeatureName:Printing-Foundation-InternetPrinting-Client /NoRestart
+Dism /online /Disable-Feature /FeatureName:Recall /NoRestart
